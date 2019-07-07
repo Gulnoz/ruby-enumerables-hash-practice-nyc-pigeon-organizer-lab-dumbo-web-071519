a78b1f3e359b51bdd341e17f6d9141a6c5024877
fortuitous-counter-8979 <gmcharmy@gmail.com>
2019-07-07 21:01:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+new_hash={}
+ data.each{|key_atr,value_atr|
+   value_atr.each{|key_param,value_param|
+     value_param.each{|i|
+      new_hash[i]={}
+      
+      }
+      }
+     }
+    #new_hash.each{|key,value| new_hash[key][key_atr]=[]}
+
+     return new_hash
 end
+
